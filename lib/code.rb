5e2f67902d8c4e0fb80b9886c8d2e657f9e375ee
hanzi-code <195461519+hanzi-code@users.noreply.github.com>
2025-04-14 10:59:32 -0300

    Edit code.rb
    Add all file logic necessary for the code to be
    created by the user and for the computer too
    added a random code generator method too

---
+++ lib/code.rb
@@ -0,0 +1,47 @@
+class Code
+  COLORS = [:red, :green, :yellow, :magenta, :blue, :cyan].freeze
+  CODE_LENGTH = 4
+
+  attr_reader :value
+
+  def initialize(value)
+    @value = value || generate_random_code
+  end
+
+  def evaluate_guess(guess)
+    exact_matches = 0
+    color_matches = 0
+
+    secret_remaining = @value.dup
+    guess_remaining = guess.value.dup
+
+    # Check exact matches
+    CODE_LENGTH.each do |i|
+      if guess.value[i] == @value[i]
+        exact_matches += 1
+        secret_remaining[i] = nil
+        guess_remaining[i] = nil
+      end
+    end
+
+    # Check color matches
+    guess_remaining.compact.each do |color|
+      if secret_remaining.include?(color)
+        color_matches += 1
+        secret_remaining[secret_remaining.index(color)] = nil
+      end
+  end
+  {exact: exact_matches, color: color_matches}
+end
+
+def to_s
+  @value.map {|color| "● ".colorize(color)}.join
+end
+
+private
+
+def generate_random_code
+  CODE_LENGTH.times.map {COLORS.sample}
+end
+
+end

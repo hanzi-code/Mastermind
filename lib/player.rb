ca970c65d034d9947536af444d8078403dbe2d4a
hanzi-code <195461519+hanzi-code@users.noreply.github.com>
2025-04-14 14:52:38 -0300

    Edit player.rb
    Add all logic for the player guess and the
    validation of his guess
    will be requiring the code.rb through here
    may this work

---
+++ lib/player.rb
@@ -0,0 +1,37 @@
+require 'colorize'
+require_relative 'code.rb'
+
+class Player
+  def get_guess
+    loop do
+      print "Enter you guess (e.g, red green blue yellow): "
+      input = gets.chomp.downcase.split.map(&:to_sym)
+
+      if valid_guess?(input)
+        return Code.new
+      else
+        puts "Invalid guess. Please enter #{Code::CODE_LENGTH} colors from: #{Code::COLORS.join(', ')}".colorize(:light_red)
+      end
+    end
+  end
+
+  def create_code
+    puts "\nCreate a secret code for the computer to guess."
+    
+    loop do
+      print "Enter your code (e.g, red green blue yellow): "
+      input = gets.chomp.downcase.split.map(&:to_sym)
+
+      if valid_guess?(input)
+        return Code.new
+      else
+        puts "Invalid code. Please enter #{Code::CODE_LENGTH} colors from: #{Code::COLORS.join(', ')}".colorize(:light_red)
+      end
+    end
+  end
+  private
+
+  def valid_guess?(guess)
+    guess.size == Code::CODE_LENGTH && guess.all? {|color| Code::COLORS.include?(color)}
+  end
+end
